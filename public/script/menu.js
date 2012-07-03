005d226c1122e694e91aac8e52a9f6fd4500a97e
murgatroid99 <michael.lumish@gmail.com>
2012-07-03 10:28:50 -0400

    Finished code to add items to tray, changed to using class selectors

--- public/script/menu.js
+++ public/script/menu.js
@@ -2,6 +2,45 @@ var ordrin = {};
 
 (function(){
   "use strict";
+
+  // ordrin api classes
+  var TrayItem = function(itemId, quantity, options){
+    this.itemId   = itemId;
+    this.quantity = quantity;
+    this.options  = options;
+
+    this.buildItemString = function(){
+      var string = this.itemId + "/" + this.quantity;
+
+      for (var i = 0; i< this.options.length; i++){
+        string += "," + this.options[i];
+      }
+      return string;
+    }
+
+  }
+
+  var Tray = function(items){
+    this.items = items;
+
+    this.buildTrayString = function(){
+      var string = "";
+      for (var i = 0; i < this.items.length; i++){
+        string += "+" + this.items[i].buildItemString();
+      }
+      return string.substring(1); // remove that first plus
+    };
+
+    this.addItem = function(item){
+      if (!(item instanceof TrayItem)){
+        throw new Error("Item must be an object of the Tray Item class");
+      } else {
+        this.items.push(item);
+      }
+    }  
+  };
+
+  ordrin.tray = new Tray([])
   
   function listen(evnt, elem, func) {
     if (elem.addEventListener)  // W3C DOM
@@ -41,7 +80,7 @@ var ordrin = {};
     var routes = {  
       menuItem    : createDialogBox,
       closeDialog : hideDialogBox,
-      deleteItem: deleteItem,
+      addToTray: addTrayItem
     }
 
     var name = event.srcElement.getAttribute("data-listener");
@@ -61,9 +100,9 @@ var ordrin = {};
     var descrip = node.getElementsByClassName("menuItemDescription")[0].innerHTML;
     var id      = node.getAttribute("data-miid");
     var category = goUntilParent(node, "menuCategory").getElementsByClassName("itemListName")[0].innerHTML;
-    document.getElementById("dialogDescription").innerHTML = descrip;
-    document.getElementById("optionsTitle").innerHTML = title;
-    var dialog = document.getElementById("optionsDialog");
+    document.getElementById("ordrinMenu").getElementsByClassName("dialogDescription")[0].innerHTML = descrip;
+    document.getElementById("ordrinMenu").getElementsByClassName("optionsTitle")[0].innerHTML = title;
+    var dialog = document.getElementById("ordrinMenu").getElementsByClassName("optionsDialog")[0];
     dialog.setAttribute("data-miid", id);
     dialog.setAttribute("data-title", title);
     dialog.setAttribute("data-category", category);
@@ -75,7 +114,7 @@ var ordrin = {};
     node.className = node.className.replace("hidden", "");
 
     // put them in the dialog option container
-    document.getElementById("optionContainer").appendChild(node);
+    document.getElementById("ordrinMenu").getElementsByClassName("optionContainer")[0].appendChild(node);
 
     showDialogBox();
   }
@@ -85,102 +124,53 @@ var ordrin = {};
       name = "optionsDialog";
     }
     // gray out background
-    var background = document.getElementById("dialogBg");
+    var background = document.getElementById("ordrinMenu").getElementsByClassName("dialogBg")[0];
     // show background
     background.className = background.className.replace("hidden", "");
 
     // show the dialog
-    var dialog = document.getElementById(name);
+    var dialog = document.getElementById("ordrinMenu").getElementsByClassName(name)[0];
     dialog.className = dialog.className.replace("hidden", "");
   }
 
   // checks if dialog is closable, and closes it if so
   function closeOptionsDialog(){
-    var textarea = document.getElementsByClassName("editBox")[0];
-    var id       = document.getElementById("optionsDialog").getAttribute('data-miid');
-    if (textarea.value === "" || textarea.value === "Does anything need to be changed?"
-        || textarea.value == menuEdits[category][id]){
-      hideDialogBox();
-    }else{
-      textarea.className += " redBorder";
-      var editError = goUntilParent(textarea, "editBoxContainer").getElementsByClassName("editError")[0];
-      editError.className = editError.className.replace("hidden", "");
-    }
+    hideDialogBox();
   }
 
   function hideDialogBox(){
-    var background     = document.getElementById("dialogBg");
-    var dialog         = document.getElementById("optionsDialog");
+    var background     = document.getElementById("ordrinMenu").getElementsByClassName("dialogBg")[0];
+    var dialog         = document.getElementById("ordrinMenu").getElementsByClassName("optionsDialog")[0];
     
     if (dialog.className.indexOf("hidden") == -1){
       // hide the background and dialog box
       background.className += " hidden";
       dialog.className     += " hidden";
       // remove elements in option container
-      var optionContainer = document.getElementById("optionContainer");
+      var optionContainer = document.getElementById("ordrinMenu").getElementsByClassName("optionContainer")[0];
       optionContainer.removeChild(optionContainer.getElementsByClassName("optionCategoryList")[0]);
-      // reset edit box
-      var editBox   = dialog.getElementsByClassName("editBox")[0];
-      editBox.value = "Does anything need to be changed?";
-      editBox.removeAttribute("disabled");
-
-      // remove red border if there
-      var editError = dialog.getElementsByClassName("editError")[0];
-      if (editError.className.indexOf("hidden") == -1){
-        editError.className += " hidden";
+      checkBoxes = dialog.getElementsByClassName("optionCheckbox");
+      for(var i=0; i<checkBoxes.length; i++){
+        checkBoxes[i].checked = false;
       }
-      editBox.className = editBox.className.replace("redBorder", "");
-
-      document.getElementById("deleteItem").checked = false;
     }
   }
 
   function addTrayItem(){
-    var dialog = document.getElementById("ordrinMenu").getElementsByClass("optionsDialog")[0]
+    var dialog = document.getElementById("ordrinMenu").getElementsByClassName("optionsDialog")[0]
     var id = dialog.getAttribute("data-miid")
-    var item = dialog.getElementsByClass("optionsTitle")[0].innerHTML
+    var item = dialog.getElementsByClassName("optionsTitle")[0].innerHTML
     var category = dialog.getAttribute("data-category")
+    
 
-    check_boxes = dialog.getElementsByClassName("optionCheckbox")
-    for(var i=0; i<check_boxes.length; i++){
-      
+    checkBoxes = dialog.getElementsByClassName("optionCheckbox")
+    options = []
+    for(var i=0; i<checkBoxes.length; i++){
+      if(checkBoxes[i].checked){
+        option = goUntilParent(checkBoxes[i], "option").getAttribute("data-moid")
+      }
     }
+    trayItem = new TrayItem(id, quantity, options)
+    ordrin.tray.addItem(trayItem)
   }
-
-  // ordrin api classes
-  var TrayItem = function(itemId, quantity, options){
-    this.itemId   = itemId;
-    this.quantity = quantity;
-    this.options  = options;
-
-    this.buildItemString = function(){
-      var string = this.itemId + "/" + this.quantity;
-
-      for (var i = 0; i< this.options.length; i++){
-        string += "," + this.options[i];
-      }
-      return string;
-    }
-
-  }
-
-  var Tray = function(items){
-    this.items = items;
-
-    this.buildTrayString = function(){
-      var string = "";
-      for (var i = 0; i < this.items.length; i++){
-        string += "+" + this.items[i].buildItemString();
-      }
-      return string.substring(1); // remove that first plus
-    };
-
-    this.addItem = function(item){
-      if (!(item instanceof TrayItem)){
-        throw new Error("Item must be an object of the Tray Item class");
-      } else {
-        this.items.push(item);
-      }
-    }  
-  };
 })();
